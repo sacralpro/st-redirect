405fa6c8919bad69d4905c05fc2817a178a330aa
Your Name <sacralprojects8@gmail.com>
2025-05-19 15:36:30 +0300

    Fix: Remove unused Image import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
